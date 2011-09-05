f0b8ad4edabbe8838513f157fb43fecf16fb15fd
Paul Blair <ciriwe@phobot.net>
2011-09-05 15:24:14 -0400

    Change lein-gaectl to use shell script so that log output can be trapped.

--- leiningen/gae.clj
+++ leiningen/gae.clj
@@ -15,15 +15,12 @@
 	(let [appserver-sdk-install-path (project :gae-appserver-sdk-install-path)
 				appserver-address (project :gae-appserver-address)
 				appserver-port (project :gae-appserver-port)]
-	(run-shell-command "java"
-										 "-ea"
-										 "-cp"
-										 (str appserver-sdk-install-path "/lib/appengine-tools-api.jar")
-										 "com.google.appengine.tools.KickStart"
-										 "com.google.appengine.tools.development.DevAppServerMain"
-										 (str "--address=" appserver-address)
-										 (str "--port=" appserver-port)
-										 "war")))
+		;; Using a shell command here instead of spawning the java process directly
+		;; so that we can capture the output in the logs/ directory
+		(run-shell-command "script/start_appserver.sh"
+											 appserver-sdk-install-path
+											 appserver-address
+											 appserver-port)))
 
 (defn kill-appserver []
 	(run-shell-command "script/kill_appserver.sh"))
